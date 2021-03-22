55ea354f18ce5f6675ee2735027f7136eeceb0b8
liort2020 <liort2020@gmail.com>
2021-03-22 09:56:55 +0200

    Initial Commit

---
+++ TodoList/TodoListApp.swift
@@ -0,0 +1,17 @@
+//
+//  TodoListApp.swift
+//  TodoList
+//
+//  Created by Lior Tal on 22/03/2021.
+//
+
+import SwiftUI
+
+@main
+struct TodoListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
